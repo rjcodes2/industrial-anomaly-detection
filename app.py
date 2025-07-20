5923f9fd49a30655eac28aec6c050f999f1ef05a
Rajeshwari <rajeshwari.sonawane29@gmail.com>
2025-07-20 19:10:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Industrial Anomaly Detection", layout="centered")
+
+st.title(" Industrial Machine Anomaly Detection")
+st.markdown("""
+Welcome to your interactive AI-powered anomaly detection dashboard.  
+Navigate through the pages on the left to:
+-  Explore data with interactive visualizations  
+-  Predict machine faults in real-time  
+-  Deploy to cloud for wider access  
+""")
+
+    
+# Footer
+st.markdown("""---""")
+st.markdown("<div style='text-align: center;'>Rajeshwari Sonawane | Microsoft AI & Azure Internship 2025</div>", unsafe_allow_html=True)
